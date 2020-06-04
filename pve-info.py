4da0a4ac6ee3c00a2b05cec5d8f885e8d5b721ad
be-engineer <cakeli@hotmail.com>
2020-06-04 10:31:19 +0800

    增加了一些内容，但是app connect事件不能触发，还没找到原因

--- pve-info.py
+++ pve-info.py
@@ -6,6 +6,7 @@ import blynktimer
 import platform #查看系统信息
 import psutil  #查看系统相关参数，如温度，内存等
 import socket #获取IP地址
+import subprocess  #执行系统命令
  
 #auth code
 BLYNK_AUTH = '4TFvpseX3BYmGhPKZ3bSW3XVpBkLBDDB'
@@ -14,6 +15,11 @@ BLYNK_AUTH = '4TFvpseX3BYmGhPKZ3bSW3XVpBkLBDDB'
 #blynk = blynklib.Blynk(BLYNK_AUTH, server='2959w71z50.qicp.vip', port=26514)
 #如果你无法实现内网穿透，可以取消下面语句的注释，可以实现本地局域网内的访问
 blynk = blynklib.Blynk(BLYNK_AUTH, server='139.155.4.138', port=8080)
+
+# last command in example - just to show error handling
+# for certain HW can be added specific commands. 'gpio readall' on PI3b for example
+ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc']
+#
 READ_PRINT_MSG = "Read Pin: V{}"
 # create timers dispatcher instance
 timer = blynktimer.Timer() 
@@ -21,40 +27,56 @@ timer = blynktimer.Timer()
 #app connect message
 APP_CONNECT_PRINT_MSG = 'App connected'
 APP_DISCONNECT_PRINT_MSG = 'App disconnected'
+WRITE_EVENT_PRINT_MSG = "Write Pin: V{} Value: '{}'"
 
+#app connected
 @blynk.handle_event('internal_acon')
 def app_connect_handler(*args):
     print(APP_CONNECT_PRINT_MSG)
-#when app connected,show host name and ip
-    @blynk.handle_event('write V0')
-    def write_virtual_pin_handler(pin, value):
-        # 获取本机计算机名称
-        hostname = socket.gethostname()
-        # 获取本机ip
-        ip = socket.gethostbyname(hostname)
-        print(WRITE_EVENT_PRINT_MSG.format(pin, ip))
-    #显示os名称
-    @blynk.handle_event('write V1')
-    def write_virtual_pin_handler(pin, value):
-        os = platform.uname()[0]
-        print(WRITE_EVENT_PRINT_MSG.format(pin,os ))
-    #显示hostname
-    @blynk.handle_event('write V2')
-    def write_virtual_pin_handler(pin, value):
-        host = platform.uname()[1]
-        print(WRITE_EVENT_PRINT_MSG.format(pin,host))
-    #显示os版本
-    @blynk.handle_event('write V3')
-    def write_virtual_pin_handler(pin, value):
-        rev = platform.uname()[2]
-        print(WRITE_EVENT_PRINT_MSG.format(pin,rev))
 
+    # 获取本机计算机名称
+    hostname = socket.gethostname()
+    # 获取本机ip
+    ip = socket.gethostbyname(hostname)
+    #print(WRITE_EVENT_PRINT_MSG.format(ip)
+    blynk.virtual_write(0, hostname+','+ip)
+    #显示os信息到terminal
 
+    os = platform.uname()
+    for info in os:
+        #print(WRITE_EVENT_PRINT_MSG.info)
+        blynk.virtual_write(1, info)
+
+#app disconnected
 @blynk.handle_event('internal_adis')
 def app_disconnect_handler(*args):
     print(APP_DISCONNECT_PRINT_MSG)
 
-WRITE_EVENT_PRINT_MSG = "Write Pin: V{} Value: '{}'"
+@blynk.handle_event('write V1')
+def write_handler(pin, values):
+    header = ''
+    result = ''
+    delimiter = '{}\n'.format('=' * 30)
+    if values and values[0] in ALLOWED_COMMANDS_LIST:
+        cmd_params = values[0].split(' ')
+        try:
+            result = subprocess.check_output(cmd_params).decode('utf-8')
+            header = '[output]\n'
+        except subprocess.CalledProcessError as exe_err:
+            header = '[error]\n'
+            result = 'Return Code: {}\n'.format(exe_err.returncode)
+        except Exception as g_err:
+            print("Command caused '{}'".format(g_err))
+    elif values and values[0] == 'help':
+        header = '[help -> allowed commands]\n'
+        result = '{}\n'.format('\n'.join(ALLOWED_COMMANDS_LIST))
+
+    # communicate with terminal if help or some allowed command
+    if result:
+        output = '{}{}{}{}'.format(header, delimiter, result, delimiter)
+        print(output)
+        blynk.virtual_write(pin, output)
+        blynk.virtual_write(pin, '\n')
 
 
 #显示cpu负载率
