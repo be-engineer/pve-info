fd76ae12b72f53842cf6eacde6b8a0b406c6a967
be-engineer <cakeli@hotmail.com>
2020-06-04 14:40:35 +0800

    完善代码，增加terminal作为命令输入窗口；增加负载占用率

--- pve-info.py
+++ pve-info.py
@@ -19,7 +19,7 @@ blynk = blynklib.Blynk(BLYNK_AUTH, server='139.155.4.138', port=8080)
 
 # last command in example - just to show error handling
 # for certain HW can be added specific commands. 'gpio readall' on PI3b for example
-ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'ip abc','clear']
+ALLOWED_COMMANDS_LIST = ['ls', 'lsusb', 'ip a', 'lspci','clear','lshw','date','df','pveam list local','pveversion','ls_release -a']
 #
 READ_PRINT_MSG = "Read Pin: V{}"
 # create timers dispatcher instance
@@ -40,13 +40,17 @@ def connect_handler():
     blynk.virtual_write(0, hostname+','+ip)
     #显示os信息到terminal
     os = platform.uname()
+    #info=",".join([m for m in os])
+    blynk.virtual_write(2, '系统信息')
+    blynk.virtual_write(2, '==============')
     for info in os:
-        print(WRITE_EVENT_PRINT_MSG.format(1,info))
-        blynk.virtual_write(1, info)
-
+        print(WRITE_EVENT_PRINT_MSG.format(2,info))
+        blynk.virtual_write(2, info)
+    blynk.virtual_write(2, '==============')
+        
 
 #显示terminal command执行结果
-@blynk.handle_event('write V1')
+@blynk.handle_event('write V2')
 def write_handler(pin, values):
     header = ''
     result = ''
@@ -61,8 +65,8 @@ def write_handler(pin, values):
             result = 'Return Code: {}\n'.format(exe_err.returncode)
         except Exception as g_err:
             print("Command caused '{}'".format(g_err))
-    elif values and values[0] == 'help':
-        header = '[help -> allowed commands]\n'
+    else:
+        header = '[Allowed commands]\n'
         result = '{}\n'.format('\n'.join(ALLOWED_COMMANDS_LIST))
 
     # communicate with terminal if help or some allowed command
@@ -73,7 +77,13 @@ def write_handler(pin, values):
         blynk.virtual_write(pin, '\n')
 #
 
-#显示cpu负载率
+#显示负载使用率
+@timer.register(vpin_num=1, interval=5, run_once=False)
+def write_to_virtual_pin(vpin_num=1):
+    value=psutil.getloadavg()
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    blynk.virtual_write(vpin_num, value)
+#显示cpu占用率
 @timer.register(vpin_num=4, interval=5, run_once=False)
 def write_to_virtual_pin(vpin_num=1):
     value=psutil.cpu_percent()
@@ -92,8 +102,17 @@ def write_to_virtual_pin(vpin_num=1):
     value=psutil.disk_usage('/')[3]
     print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
     blynk.virtual_write(vpin_num, value)
+@timer.register(vpin_num=7, interval=5, run_once=False)
+def write_to_virtual_pin(vpin_num=1):
+    value=psutil.disk_usage('/mnt/sdb1')[3]
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    blynk.virtual_write(vpin_num, value)    
 #显示温度    
-
+@timer.register(vpin_num=8, interval=5, run_once=False)
+def write_to_virtual_pin(vpin_num=1):
+    value=psutil.sensors_temperatures()
+    print(WRITE_EVENT_PRINT_MSG.format(vpin_num, value))
+    blynk.virtual_write(vpin_num, value)  
 
 ###########################################################
 # infinite loop that waits for event
